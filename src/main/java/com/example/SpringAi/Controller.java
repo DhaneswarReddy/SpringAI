c9198bb527707e4eaae61671c067d93998682398
DhaneswarReddy <dhaneswarreddykambham@gmail.com>
2026-07-15 21:45:16 +0530

    integratedOllamaModel

--- src/main/java/com/example/SpringAi/Controller.java
+++ src/main/java/com/example/SpringAi/Controller.java
@@ -1,6 +1,11 @@
 package com.example.SpringAi;
 
 
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.api.OllamaChatOptions;
+import org.springframework.ai.ollama.api.OllamaModel;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +18,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 	
 	private OpenAiChatModel chatmodel;
+	private OllamaChatModel ollamaChatModel;
 	
-	public Controller(OpenAiChatModel chatmodel) {
+	public Controller(OpenAiChatModel chatmodel, OllamaChatModel ollamaChatModel) {
 		this.chatmodel = chatmodel;
+		this.ollamaChatModel = ollamaChatModel;
 	}
 	
-	@GetMapping("/{message}")
+	@GetMapping("/openai/{message}")
 	public ResponseEntity<String> openAi(@PathVariable String message) {
 		
 		String response = chatmodel.call(message);
 		return ResponseEntity.ok(response);
+	}
+
+    @GetMapping("/ollama/{message}")
+	public ResponseEntity<String> ollamaChatModel(@PathVariable String message) {
+		ChatResponse response = ollamaChatModel.call(
+    new Prompt(
+        message,
+        OllamaChatOptions.builder()
+            .model(OllamaModel.LLAMA3_2_3B)
+            .temperature(0.4)
+            .build()
+    ));
+		return ResponseEntity.ok(response.getResult().getOutput().getText());
 	}
 }
